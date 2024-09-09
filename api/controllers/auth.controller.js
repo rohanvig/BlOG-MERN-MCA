93064a455431079a9856212e0a3c6f3801930cab
rohanvig <159570950+rohanvig@users.noreply.github.com>
2024-09-09 12:42:04 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -199,7 +199,7 @@ export const google = async (req, res, next) => {
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
-        .cookie("access_token", token, { httpOnly: true })
+        .cookie("access_token", token, { httpsOnly: true })
         .json(rest);
 
       // Send welcome email to new users
